9676e3a83d8fed1da67a2e0086fd94465663ed96
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2024-10-23 10:35:22 +0200

    Create an application

--- src/elm/gauge/src/Control.elm
+++ src/elm/gauge/src/Control.elm
@@ -1,15 +1,68 @@
 module Control exposing (main)
 
+import Browser exposing (Document, UrlRequest)
+import Browser.Navigation as Navigation exposing (Key)
 import Control.Gauge as Gauge exposing (Gauge)
 import Html exposing (Html)
+import Url exposing (Url)
 
 
-main : Html msg
+main : Program () Model Msg
 main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = onUrlRequest
+        , onUrlChange = onUrlChange
+        }
+
+
+type alias Model =
+    Gauge
+
+
+type Msg
+    = None
+
+
+init : flags -> Url -> Key -> ( Model, Cmd Msg )
+init _ _ _ =
     let
         gauge =
             Gauge.create 30 70
     in
-    Html.div []
-        [ Gauge.view gauge
+    ( gauge, Cmd.none )
+
+
+view : Model -> Document Msg
+view model =
+    { title = "Gauge Control"
+    , body =
+        [ Html.div
+            []
+            [ Gauge.view model
+            ]
         ]
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+onUrlRequest : UrlRequest -> Msg
+onUrlRequest _ =
+    None
+
+
+onUrlChange : Url -> Msg
+onUrlChange _ =
+    None
